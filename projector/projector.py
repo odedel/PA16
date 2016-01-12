fdc2d855cc5cafe7a83dc8b295f49573086c11c6
Oded <odedelbaz@gmail.com>
2016-01-12 21:18:39 +0200

    building 'project' function

--- projector/projector.py
+++ projector/projector.py
@@ -1,7 +1,30 @@
 __author__ = 'Odedz'
 
+def create_graph(original_code):
+    pass
+
+
+def create_projected_variable_path(program_graph, projected_variable):
+    pass
+
+
+def build_program(projected_path):
+    pass
+
+
+def project(original_code, projected_variable):
+    program_graph = create_graph(original_code)
+    projected_path = create_projected_variable_path(program_graph, projected_variable)
+    return build_program(projected_path)
+
+
 def main():
-    print 'Hello World'
+    with file(r'..\Tests\test1.py') as f:
+        original_code = f.read()
+
+    projected_code = project(original_code, 'z')
+
+    print projected_code
 
 if __name__ == '__main__':
     main()
