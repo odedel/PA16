5d0384db1029c0073031d8f3836feabe6f8234b5
Oded <odedelbaz@gmail.com>
2016-01-14 21:54:14 +0200

    Remove global vars

--- projector/projector.py
+++ projector/projector.py
@@ -2,8 +2,8 @@ import ast
 import astor
 import graphviz
 
-graph_nodes = []
-graph_edges = []
+from collections import namedtuple
+
 
 class GraphNode(object):
     def __init__(self, statement, assigned_var, influence_vars=[]):
@@ -18,6 +18,10 @@ class GraphNode(object):
 
 class ASTWalker(ast.NodeVisitor):
 
+    def __init__(self, graph_nodes=[]):
+        self.graph_nodes = graph_nodes
+        super(ASTWalker, self).__init__()
+
     def visit_Assign(self, node):
         target = node.targets[0].id
         code = astor.codegen.to_source(node)
@@ -30,15 +34,16 @@ class ASTWalker(ast.NodeVisitor):
         elif isinstance(node.value, ast.Name):
             influence_vars.append(node.value.id)
 
-        graph_nodes.append(GraphNode(code, target, influence_vars))
+        self.graph_nodes.append(GraphNode(code, target, influence_vars))
 
 
 def create_graph(original_code):
-    ast_tree = ast.parse(original_code)
-    ASTWalker().visit(ast_tree)
+    walker = ASTWalker()
+    walker.visit(ast.parse(original_code))
 
     seen_variables_to_line_code = {}
-    for enumeration, node in enumerate(graph_nodes):
+    graph_edges = []
+    for enumeration, node in enumerate(walker.graph_nodes):
         # Create edges
         for dependency in node.influence_vars:
             graph_edges.append((seen_variables_to_line_code[dependency], enumeration))
@@ -46,22 +51,22 @@ def create_graph(original_code):
         # Update the seen list
         seen_variables_to_line_code[node.assigned_var] = enumeration
 
-
-    return graph_edges
+    ProgramGraph = namedtuple('ProgramGraph', ['nodes', 'edges'])
+    return ProgramGraph(walker.graph_nodes, graph_edges)
 
 
 
 def create_projected_variable_path(program_graph, projected_variable):
     mappy = {}
     required = set()
-    for x,y in program_graph:
+    for x, y in program_graph.edges:
         if y not in mappy:
             mappy[y] = []
         mappy[y].append(x)
 
-    for i in xrange(len(graph_nodes)):
-        pos = len(graph_nodes) - i - 1
-        g = graph_nodes[pos]
+    for i in xrange(len(program_graph.nodes)):
+        pos = len(program_graph.nodes) - i - 1
+        g = program_graph.nodes[pos]
         if g.assigned_var is projected_variable or pos in required:
             required.add(pos)
             if pos in mappy:
@@ -71,17 +76,17 @@ def create_projected_variable_path(program_graph, projected_variable):
     return sorted(required)
 
 
-def build_program(projected_path):
+def build_program(program_graph, projected_path):
     program = []
     for i in projected_path:
-        program.append(graph_nodes[i].statement)
+        program.append(program_graph.nodes[i].statement)
     return program
 
 
 def project(original_code, projected_variable):
     program_graph = create_graph(original_code)
     projected_path = create_projected_variable_path(program_graph, projected_variable)
-    return build_program(projected_path)
+    return build_program(program_graph, projected_path)
 
 
 def main():
@@ -94,12 +99,12 @@ def main():
         print i
 
 
-def print_graph_nodes():
-    print "%s\t%s%s" % ("assigned".ljust(10), "influenced by".ljust(20), "statement")
-    for g in graph_nodes:
-        print "%s\t%s%s" % (str(g.assigned_var).ljust(10), str(g.influence_vars).ljust(20), g.statement)
+# def print_graph_nodes():
+#     print "%s\t%s%s" % ("assigned".ljust(10), "influenced by".ljust(20), "statement")
+#     for g in graph_nodes:
+#         print "%s\t%s%s" % (str(g.assigned_var).ljust(10), str(g.influence_vars).ljust(20), g.statement)
 
 
 if __name__ == '__main__':
     main()
-    print_graph_nodes()
+    # print_graph_nodes()
