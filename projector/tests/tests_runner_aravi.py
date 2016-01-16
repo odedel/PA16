49c42ddb6102087112f82eac39305bcc7b4efb64
Oded <odedelbaz@gmail.com>
2016-01-16 12:28:43 +0200

    Aravi

---
+++ projector/tests/tests_runner_aravi.py
@@ -0,0 +1,34 @@
+from projector import projector
+import test1
+
+if __name__ == '__main__':
+    checks = [test1]
+    for test in checks:
+        print 'Checking', test.name, '...'
+        graph = projector.create_graph(test.code)
+
+        passed = True
+        for edge in graph.control_edges:
+            if (edge.from_, edge.to) not in test.control_edges:
+                print 'Extra Control Edge: ', (edge.from_, edge.to)
+                passed = False
+        for edge in test.control_edges:
+            if not filter(lambda x: x.from_ == edge[0] and x.to == edge[1], graph.control_edges):
+                print 'Missing Control Edge: ', edge
+                passed = False
+
+        if passed:
+            print 'Pass Control Edges'
+
+        passed = True
+        for edge in graph.dep_edges:
+            if (edge.from_, edge.to) not in test.dep_edges:
+                print 'Extra Dep Edge: ', (edge.from_, edge.to)
+                passed = False
+        for edge in test.dep_edges:
+            if not filter(lambda x: x.from_ == edge[0] and x.to == edge[1], graph.dep_edges):
+                print 'Missing Dep Edge: ', edge
+                passed = False
+
+        if passed:
+            print 'Pass Dep Edges'
