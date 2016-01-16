22901567b91140c877fb00a59306fc201a638fb7
Oded <odedelbaz@gmail.com>
2016-01-16 14:45:05 +0200

    Add test

--- projector/tests/tests_runner_aravi.py
+++ projector/tests/tests_runner_aravi.py
@@ -1,8 +1,8 @@
 from projector import projector
-import test1, test3, test4, test5, test6, test7
+import test1, test3, test4, test5, test6, test7, test8
 
 if __name__ == '__main__':
-    checks = [test1, test3, test4, test5, test6, test7]
+    checks = [test1, test3, test4, test5, test6, test7, test8]
     for test in checks:
         print 'Checking', test.name, '...'
         graph = projector.create_graph(test.code)
