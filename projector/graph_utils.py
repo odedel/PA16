4e9fc7929e2bc9d60505c2d2b3d2ebe0b4831fa9
Tomer Greenwald <tomerg@gmail.com>
2016-01-16 18:52:48 +0200

    visualizing both graphs interleaved

--- projector/graph_utils.py
+++ projector/graph_utils.py
@@ -39,27 +39,31 @@ def apply_styles(graph, styles):
     return graph
 
 def visualize(graph):
-    visualize_edges(graph.control_edges, graph.nodes)
+    visualize_edges(graph,)
 
 
-def visualize_edges(edges, nodes):
+def visualize_edges(graph):
+    nodes = graph.nodes
     dot = Digraph(comment='Graph')
     #nodes = []
-    for edge in edges:
-        x, y = edge.from_, edge.to
-        # x_node = pydot.Node("Node B", style="filled", fillcolor="green")
-        if nodes[x].statement == 'if (t < x):':
-            i = 1
-        dot.node(str(x), nodes[x].statement, color="black")
-        print nodes[x].statement
-        if y < len(nodes):
-            dot.node(str(y), nodes[y].statement)
-            if edge.jmp_true is None:
-                dot.edge(str(x), str(y), color="black")
-            elif edge.jmp_true is True:
-                dot.edge(str(x), str(y), color="blue")
-            else:
-                dot.edge(str(x), str(y), color="red")
+    for edges, is_control in [(graph.control_edges, True), (graph.dep_edges, False)]:
+        for edge in edges:
+            x, y = edge.from_, edge.to
+            # x_node = pydot.Node("Node B", style="filled", fillcolor="green")
+            if nodes[x].statement == 'if (t < x):':
+                i = 1
+            dot.node(str(x), nodes[x].statement, color="black")
+            print nodes[x].statement
+            if y < len(nodes):
+                dot.node(str(y), nodes[y].statement)
+                if not is_control:
+                    dot.edge(str(x), str(y), color="green")
+                elif edge.jmp_true is None:
+                    dot.edge(str(x), str(y), color="black")
+                elif edge.jmp_true is True:
+                    dot.edge(str(x), str(y), color="blue")
+                else:
+                    dot.edge(str(x), str(y), color="red")
     # apply_styles(dot, styles)
     dot.render(r"T:\out.gv", view=False)
 
